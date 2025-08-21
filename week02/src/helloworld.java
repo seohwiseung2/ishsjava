0e01a640aa100f03ddd6db4689ac3947c2244d75
seohwiseung2 <seohwiseung2@gmail.com>
2025-08-21 21:31:25 +0900

    Vo3 HelloWorld

---
+++ week02/src/helloworld.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class helloworld {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.print("안녕" + name);
+    }
+}
