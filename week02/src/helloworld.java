874968b77e05768239deea7008e5813c97cae85e
seohwiseung2 <seohwiseung2@gmail.com>
2025-08-28 19:28:05 +0900

    v0.4 README.md

--- week02/src/helloworld.java
+++ week02/src/helloworld.java
@@ -4,6 +4,6 @@ public class helloworld {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String name = scanner.next();
-        System.out.print("안녕" + name);
+        System.out.print("안녕" + name);:
     }
 }
