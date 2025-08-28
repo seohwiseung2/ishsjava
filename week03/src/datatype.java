d1d8d9c71d4a74878abe28b3cb42134e9caa53fb
seohwiseung2 <seohwiseung2@gmail.com>
2025-08-28 21:02:48 +0900

    v0.6 data and casting

--- week03/src/datatype.java
+++ week03/src/datatype.java
@@ -1,5 +1,6 @@
 public class datatype {
     public static void main(String[] args) {
+        //데이터 타입과 형변화
         int score = 87;
         float gpa = 4.13F;  //float는 4바이트, double은 8바이트 둘다 실수임
         System.out.println(score);
