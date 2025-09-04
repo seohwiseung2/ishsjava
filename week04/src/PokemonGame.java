8ae9f275a726ff474f4aac25368bec290482d0ef
seohwiseung2 <seohwiseung2@gmail.com>
2025-09-04 20:30:24 +0900

    v1.3

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -1,4 +1,5 @@
 import java.nio.channels.SelectionKey;
+import java.util.Scanner;
 
 class Pokemon{
     //concrete class (can produce object)
@@ -9,11 +10,21 @@ class Pokemon{
 public class PokemonGame {
     public static void main(String[] args) {
         Pokemon pikachu = new Pokemon();
-        Pokemon squirtle = new Pokemon();
-        pikachu.hp = 100;
-        squirtle.hp = 110;
-        System.out.println(pikachu.hp);
-        System.out.println(squirtle.hp);
+       // Pokemon squirtle = new Pokemon();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("체력을 입력하세요 :");
+        pikachu.hp = scanner.nextInt();
+
+      //  squirtle.hp = 110;
+      //  System.out.println(pikachu.hp);
+       // System.out.println(squirtle.hp);
+
+        if(pikachu.hp > 99) {
+            System.out.println("피카츄의 체력이 여유" + pikachu.hp);
+        }else{
+            System.out.println("피카츄의 체력(" + pikachu.hp +")이 부족!");
+        }
 
     }
 
