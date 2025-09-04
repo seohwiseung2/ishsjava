47fc8541ecd70f6786d9e70694501c57051c61e2
seohwiseung2 <seohwiseung2@gmail.com>
2025-09-04 20:39:52 +0900

    v1.4 if문

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -10,20 +10,25 @@ class Pokemon{
 public class PokemonGame {
     public static void main(String[] args) {
         Pokemon pikachu = new Pokemon();
-       // Pokemon squirtle = new Pokemon();
+        Pokemon squirtle = new Pokemon();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("체력을 입력하세요 :");
+        Scanner scanner = new Scanner(System.in);  //파이선 인풋
+        System.out.print("피카츄의 체력을 입력하세요 :");
         pikachu.hp = scanner.nextInt();
 
-      //  squirtle.hp = 110;
+       squirtle.hp = 110;
       //  System.out.println(pikachu.hp);
        // System.out.println(squirtle.hp);
 
-        if(pikachu.hp > 99) {
+        if(pikachu.hp > squirtle.hp) {
             System.out.println("피카츄의 체력이 여유" + pikachu.hp);
-        }else{
+            System.out.println("꼬부기의 체력(" + squirtle.hp +")보다 높아용ㅇ");
+        }else if(pikachu.hp < squirtle.hp){
             System.out.println("피카츄의 체력(" + pikachu.hp +")이 부족!");
+            System.out.println("꼬부기의 체력(" + squirtle.hp +")보다 낮아용");
+
+        }else {
+            System.out.println("체력동일");
         }
 
     }
