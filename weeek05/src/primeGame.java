3c8e1440d2bf0c874bcd0347c8d5466b5e961478
seohwiseung2 <seohwiseung2@gmail.com>
2025-09-11 19:09:47 +0900

    222

---
+++ weeek05/src/primeGame.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class primeGame {
+    public static void main(String[] args) {
+        //System.out.println(Math.sqrt(25.0)); //수학기호 불러오는법 루트
+
+
+
+        Scanner sc = new Scanner(System.in);
+        int counter = 0;
+        int number = sc.nextInt();
+
+        for(int i=2; i<=Math.sqrt(number); i=i+1){
+            if(number % i == 0){
+                counter++;  //counter = counter + 1
+                break;
+            }
+            System.out.print(i + " ");
+        }
+        if(counter == 0){
+            System.out.println(number + " is a prime number");
+        }else{
+            System.out.println(number + " is not a prime number");
+        }
+    }
+}
